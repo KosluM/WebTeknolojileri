2951e44dcfaf12b307387fd6934d60f566baa0de
KosluM <96433039+KosluM@users.noreply.github.com>
2022-05-09 22:42:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <div class="form-container">
+            <h1>Giriş Ekranı</h1>
+            
+            <form action="hakkında.html" method="_POST">
+                <div class="control">
+                    <label>Kullanıcı Adı</label>
+                    <input type="text" name="username" id="name">
+                </div>
+                <div class="control">
+                    <label >Şifre</label>
+                    <input type="password" name="password" id="sifre">
+               
+                <input type="submit" name="giris" value="Giriş">
+            </div>
+        </form>
+        </div>
+    </section>
+    
+
+</body>
+</html>
